4fe27b9f624921883e2bb022336dcead530cbe6b
Karis Sponsler <karis.sponsler@apptreesoftware.com>
2017-07-22 22:14:43 -0700

    removed unnecessary comments

--- Physics.js
+++ Physics.js
@@ -1,27 +1,26 @@
 function Physics() {
     this.position = new Position(0, 0, 0);
+    this.orientation = new Orientation(90);
     this.angularDrag = 0;
     this.drag = 0;
     var self = this;
 
-    /*
-        *** BUILDER FUNCTIONS ***
-    These functions allow you to build a Physics object on creation
-    as in: var physics = new Physics().withPosition(0, 0, 0).withDrag(.03);
-    They change the current instance and return it. This works because the constructor
-    function returns itself at the end.
-    */
-    this.withPosition = function(x, y, z) {
+    this.update() = function() {
+        this.position.vector.applyDrag(drag);
+        this.orientation.applyAngularDrag(angularDrag);
+        this.position.update();
+        this.orientation.update();
+    }
+
+    this.withPosition = function(x, y) {
         self.x = x;
         self.y = y;
-        self.z = z;
         return self;
     }
 
-    this.withVector = function(x, y, z) {
+    this.withVector = function(x, y) {
         self.position.vector.x = x;
         self.position.vector.y = y;
-        self.position.vector.z = z;
         return self;
     }
 
@@ -35,52 +34,89 @@ function Physics() {
         return self;
     }
 
+    this.withOrientation = function(angle) {
+        self.orientation.angle = angle;
+        return self;
+    }
+
     return this;
 }
 
-function Vector(x, y, z) {
+function Vector(x, y) {
     this.x = x;
     this.y = y;
-    this.z = z;
+    this.dragTolerance = 0;
     var self = this;
 
     this.add = function(vector) {
         self.x += vector.x;
         self.y += vector.y;
-        self.z += vector.z;
     }
 
     this.combine = function(vector) {
-        return new Vector(self.x + vector.x, self.y + vector.y, self.z + vector.z);
+        return new Vector(self.x + vector.x, self.y + vector.y);
+    }
+
+    this.applyDrag = function(drag) {
+        self.x -= (self.x * drag);
+        self.y -= (self.y * drag);
+        if(self.x <= self.dragTolerance) self.x = 0;
+        if(self.y <= self.dragTolerance) self.y = 0;
     }
 }
 
-function Position(x, y, z) {
+function Position(x, y) {
     this.x = x;
     this.y = y;
-    this.z = z;
-    this.vector = new Vector(0, 0, 0);
+    this.vector = new Vector(0, 0);
     var self = this;
 
     this.update = function() {
         self.x = ((self.x + self.vector.x) < 0) ? 0 : self.x + self.vector.x;
-        self.x = ((self.y + self.vector.y) < 0) ? 0 : self.y + self.vector.y;
-        self.x = ((self.z + self.vector.z) < 0) ? 0 : self.z + self.vector.z;        
+        self.y = ((self.y + self.vector.y) < 0) ? 0 : self.y + self.vector.y;
     }
 
-    this.updateWithPosition = function(x, y, z) {
+    this.updateWithPosition = function(x, y) {
         self.x = x;
         self.y = y;
-        self.z = z;
     }
 
-    var applyVector = function(vector) {
+    this.applyVector = function(vector) {
         self.x = ((self.x + vector.x) < 0) ? 0 : self.x + vector.x;
-        self.x = ((self.y + vector.y) < 0) ? 0 : self.y + vector.y;
-        self.x = ((self.z + vector.z) < 0) ? 0 : self.z - vector.z;
+        self.y = ((self.y + vector.y) < 0) ? 0 : self.y + vector.y;
         self.vector = vector;       
+    }
+
+    this.reverseYVector = function() {
+        self.vector.y *= -1;
+    }
+
+    this.reverseXVector = function() {
+        self.vector.x *= -1;
     }
 }
 
-var physics = new Physics().withPosition(3, 4, 5).withDrag(.025);
-document.getElementById("content").innerHTML = "(" + physics.position.x + ", " + physics.position.y + ", " + physics.position.z + "), " + physics.drag;
+function Orientation(angle) {
+    this.angle = 90;
+    this.force = 0;
+    this.dragTolerance = 0;
+    var self = this;
+
+    this.update = function() {
+        self.angle += self.force;
+        if(self.angle >= 360) self.angle -= 360;
+        else if(self.angle < 0) self.angle += 360;
+    }
+
+    this.applyForce = function(force) {
+        self.force = force;
+        self.angle += force;
+        if(self.angle >= 360) self.angle -= 360;
+        else if(self.angle < 0) self.angle += 360;
+    }
+
+    this.applyAngularDrag = function(angularDrag) {
+        self.force -= (self.force * angularDrag);
+        if(self.force <= self.dragTolerance) self.force = 0;
+    }
+}
