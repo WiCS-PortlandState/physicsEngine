befbb567e17008d08f6a40164fc0aee2b1455470
Karis Sponsler <karis.sponsler@apptreesoftware.com>
2017-07-23 17:15:32 -0700

    comments on Physics.js

--- Physics.js
+++ Physics.js
@@ -1,16 +1,31 @@
+/*
+This class determines how an Object moves through the viewport. It deals with the vector forces 
+acting on it and how those forces react over time. Both linear and angular forces are taken into 
+account. It should be used by calling update at specified intervals to make an object move through 
+its environment.
+*/
 function Physics() {
     this.position = new Position(0, 0, 0);
-    this.orientation = new Orientation(90);
+    this.orientation = new Orientation();
     this.angularDrag = 0;
     this.drag = 0;
     var self = this;
 
+    /*
+    INPUT: n/a
+    OUTPUT: n/a
+    Updates the current position and orientation considering the forces being applied to 
+    them. Also updates the force vectors to account for drag after updating the position and 
+    rotation.
+    */
     this.update() = function() {
-        this.position.vector.applyDrag(drag);
-        this.orientation.applyAngularDrag(angularDrag);
         this.position.update();
         this.orientation.update();
+        this.position.vector.applyDrag(drag);
+        this.orientation.applyAngularDrag(angularDrag);
     }
+
+    // Builder functions
 
     this.withPosition = function(x, y) {
         self.x = x;
@@ -39,9 +54,24 @@ function Physics() {
         return self;
     }
 
+    this.withDragTolerance = function(tolerance) {
+        self.position.withDragTolerance(tolerance);
+        return self;
+    }
+
+    this.withAngularDragTolerance = function(tolerance) {
+        self.angle.withAngularDragTolerance(tolerance);
+        return self;
+    }
+
     return this;
 }
 
+/*
+This class represents a force Vector acting on the linear position of an Object. 
+The drag tolerance is a number that describes when a force should reduce to zero rather 
+than slow any further (or else a vector would never truely reach zero unless instantiated that way).
+*/
 function Vector(x, y) {
     this.x = x;
     this.y = y;
@@ -57,34 +87,66 @@ function Vector(x, y) {
         return new Vector(self.x + vector.x, self.y + vector.y);
     }
 
+    /*
+    INPUT: drag coefficient
+    OUTPUT: n/a
+    Adjusts the current Vector to account for the given drag coefficient. If 
+    the resulting Vector has a member within the drag tolerance, it is reduced to zero.
+    */
     this.applyDrag = function(drag) {
         self.x -= (self.x * drag);
         self.y -= (self.y * drag);
-        if(self.x <= self.dragTolerance) self.x = 0;
-        if(self.y <= self.dragTolerance) self.y = 0;
+        if(Math.abs(self.x) <= self.dragTolerance) self.x = 0;
+        if(Math.abs(self.y) <= self.dragTolerance) self.y = 0;
     }
+
+    // Builder functions
+
+    this.withDragTolerance = function(tolerance) {
+        self.dragTolerance = tolerance;
+        return self;
+    }
+
+    return this;
 }
 
+/*
+This class tracks the position of an Object in the viewport. It uses drag to track 
+the decay of linear force.
+*/
 function Position(x, y) {
     this.x = x;
     this.y = y;
     this.vector = new Vector(0, 0);
     var self = this;
 
+    /*
+    INPUT: n/a
+    OUTPUT: n/a
+    Updates the current position. The current position cannot be < 0.
+    */
     this.update = function() {
         self.x = ((self.x + self.vector.x) < 0) ? 0 : self.x + self.vector.x;
         self.y = ((self.y + self.vector.y) < 0) ? 0 : self.y + self.vector.y;
     }
 
+    /*
+    INPUT: (x, y) coordinates
+    OUTPUT: n/a
+    Replaces the current instance's position.
+    */
     this.updateWithPosition = function(x, y) {
         self.x = x;
         self.y = y;
     }
 
-    this.applyVector = function(vector) {
-        self.x = ((self.x + vector.x) < 0) ? 0 : self.x + vector.x;
-        self.y = ((self.y + vector.y) < 0) ? 0 : self.y + vector.y;
-        self.vector = vector;       
+    /*
+    INPUT: Vector to add to this instance
+    OUTPUT: n/a
+    Adds the given Vector to the instance's.
+    */
+    this.addVector = function(vector) {
+        self.vector.add(vector);
     }
 
     this.reverseYVector = function() {
@@ -94,29 +156,71 @@ function Position(x, y) {
     this.reverseXVector = function() {
         self.vector.x *= -1;
     }
+
+    // Builder functions
+
+    this.withVector = function(vector) {
+        self.vector = vector;
+        return self;
+    }
+
+    this.withDragTolerance = function(tolerance) {
+        self.vector.dragTolerance = tolerance;
+        return self;
+    }
+
+    return this;
 }
 
-function Orientation(angle) {
+/*
+This class tracks the rotational position of an Object in degrees. It uses angular drag to 
+manage the decay of rotational forces.
+*/
+function Orientation() {
     this.angle = 90;
     this.force = 0;
     this.dragTolerance = 0;
     var self = this;
 
+    /*
+    INPUT: n/a
+    OUTPUT: n/a
+    Updates the rotational position based on the force. If the position is 
+    outside 0 - 360, it is brought back within these bounds.
+    */
     this.update = function() {
         self.angle += self.force;
         if(self.angle >= 360) self.angle -= 360;
         else if(self.angle < 0) self.angle += 360;
     }
 
-    this.applyForce = function(force) {
-        self.force = force;
-        self.angle += force;
-        if(self.angle >= 360) self.angle -= 360;
-        else if(self.angle < 0) self.angle += 360;
-    }
-
+    /*
+    INPUT: Angular drag coefficient
+    OUTPUT: n/a
+    Updates the current force to reflect the angular drag given. If the resulting force 
+    is within the drag tolerance, it is reduced to zero.
+    */
     this.applyAngularDrag = function(angularDrag) {
         self.force -= (self.force * angularDrag);
-        if(self.force <= self.dragTolerance) self.force = 0;
+        if(Math.abs(self.force) <= self.dragTolerance) self.force = 0;
     }
+
+    // Builder functions
+
+    this.withAngle = function(angle) {
+        self.angle = angle;
+        return self;
+    }
+
+    this.withForce = function(force) {
+        self.force = force;
+        return self;
+    }
+
+    this.withDragTolerance = function(tolerance) {
+        self.dragTolerance = tolerance;
+        return self;
+    }
+
+    return this;
 }
